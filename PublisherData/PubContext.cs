b1bd197e33c99fb53b4ff495638cf65ff541d9d9
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-02-22 19:27:12 +0100

    Add project files.

---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PublisherDomain;
+using System.ComponentModel;
+
+namespace PublisherData
+{
+	public class PubContext : DbContext
+	{
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Author> Authors { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cover> Covers { get; set; }
+        public PubContext()
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PubDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;")
+			   .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+			   .LogTo(Console.WriteLine,
+					new[] { DbLoggerCategory.Database.Command.Name },
+					LogLevel.Information
+					)
+			   .EnableSensitiveDataLogging(true);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Author>().HasMany<Book>().WithOne();
+		}
+	}
+}
